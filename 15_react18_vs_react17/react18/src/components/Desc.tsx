7dc979b8f2010f3cecce7cd1e33d05d0127c993a
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-12-06 11:26:21 +0900

    15_react18_vs_react17

--- 15_react18_vs_react17/react18/src/components/Desc.tsx
+++ 15_react18_vs_react17/react18/src/components/Desc.tsx
@@ -11,6 +11,7 @@ const Desc = ({ title, children }: Props) => {
       <h1 className="display-6">{title}</h1>
       <div className="p-3 mb-3 rounded" style={{ backgroundColor: '#f3f4f6' }}>
         {React.Children.toArray(children).map((child) => child)}
+        {/* 単純にこれでもOK {children} */}
       </div>
     </>
   );
