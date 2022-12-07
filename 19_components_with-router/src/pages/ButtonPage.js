64ddeaa722992ea2f2450443fe2bd356bd0db273
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-12-07 15:23:54 +0900

    19_components_with-router

---
+++ 19_components_with-router/src/pages/ButtonPage.js
@@ -0,0 +1,98 @@
+import {
+  BsFillGeoAltFill,
+  BsFillPencilFill,
+  BsFillSkipBackwardFill,
+  BsArrowRightSquare,
+} from 'react-icons/bs';
+import Button from '../components/Button';
+
+const ButtonPage = () => {
+  return (
+    <div className="p-2 flex gap-3">
+      <div>
+        <Button
+          className="mb-1 block"
+          primary
+          onClick={() => console.log('Click')}
+        >
+          <BsFillGeoAltFill />
+          登録
+        </Button>
+        <Button className="mb-1 block" primary rounded>
+          <BsFillPencilFill />
+          登録
+        </Button>
+        <Button className="mb-1 block" primary rounded outline>
+          <BsFillSkipBackwardFill />
+          登録
+        </Button>
+        <Button className="mb-1 block" primary outline>
+          <BsArrowRightSquare />
+          登録
+        </Button>
+      </div>
+
+      <div>
+        <Button className="mb-1 block" secondary>
+          登録
+        </Button>
+        <Button className="mb-1 block" secondary rounded>
+          登録
+        </Button>
+        <Button className="mb-1 block" secondary rounded outline>
+          登録
+        </Button>
+        <Button className="mb-1 block" secondary outline>
+          登録
+        </Button>
+      </div>
+
+      <div>
+        <Button className="mb-1 block" success>
+          登録
+        </Button>
+        <Button className="mb-1 block" success rounded>
+          登録
+        </Button>
+        <Button className="mb-1 block" success rounded outline>
+          登録
+        </Button>
+        <Button className="mb-1 block" success outline>
+          登録
+        </Button>
+      </div>
+
+      <div>
+        <Button className="mb-1 block" warning>
+          登録
+        </Button>
+        <Button className="mb-1 block" warning rounded>
+          登録
+        </Button>
+        <Button className="mb-1 block" warning rounded outline>
+          登録
+        </Button>
+        <Button className="mb-1 block" warning outline>
+          登録
+        </Button>
+      </div>
+
+      <div>
+        <Button className="mb-1 block" danger>
+          登録
+        </Button>
+        <Button className="mb-1 block" danger rounded>
+          登録
+        </Button>
+        <Button className="mb-1 block" danger rounded outline>
+          登録
+        </Button>
+        <Button className="mb-1 block" danger outline>
+          登録
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonPage;
