7d9e9e6726fffd738c1063928bda68ad900fce74
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-12-09 07:36:38 +0900

    modal

--- 19_components_with-router/src/components/Modal.js
+++ 19_components_with-router/src/components/Modal.js
@@ -1,5 +1,34 @@
-const Modal = () => {
-  return <div>Modal</div>;
+import { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+const Modal = ({ children, actionBar, onClose }) => {
+  useEffect(() => {
+    // modalを開いている間はスクロールできないようにしたいので、bodyにoverflow-hiddenをつける。
+    document.querySelector('body').classList.add('overflow-hidden');
+
+    return () => {
+      document.querySelector('body').classList.remove('overflow-hidden');
+    };
+  }, []);
+
+  return ReactDOM.createPortal(
+    <div>
+      <div
+        onClick={onClose}
+        className="fixed inset-0 bg-gray-300 opacity-80"
+      ></div>
+
+      {/* overlayと同じ並びにあることがポイント。overlayの中にあると、イベントバブリングが効いて、背景白部分をクリックしてもonCloseが動いてしまう。 */}
+      {/* absoluteにすると、画面をスクロールした状態でモーダルを開くとうまくいかないので、結局はfixedを使う。 */}
+      <div className="fixed inset-40 p-5 bg-white rounded-md">
+        <div className="flex flex-col justify-between h-full">
+          {children}
+          <div className="flex justify-end">{actionBar}</div>
+        </div>
+      </div>
+    </div>,
+    document.querySelector('.modal-container')
+  );
 };
 
 export default Modal;
--- 19_components_with-router/src/pages/ModalPage.js
+++ 19_components_with-router/src/pages/ModalPage.js
@@ -1,9 +1,83 @@
+import { useState } from 'react';
+import Button from '../components/Button';
 import Modal from '../components/Modal';
 
 const ModalPage = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <Button primary onClick={handleClose}>
+      I Accept
+    </Button>
+  );
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <div>
+        <p>Here is an important agreement for you to accept.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus
+          quibusdam deserunt neque sed nam ipsa, doloremque ullam! Sed,
+          asperiores similique?
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem alias
+          consectetur sit fuga nihil est!
+        </p>
+      </div>
+    </Modal>
+  );
+
+  const lorem = (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae vel
+      pariatur autem laudantium sequi illo quibusdam quas veritatis tempore
+      reprehenderit quae nulla earum, reiciendis incidunt velit eaque quo
+      adipisci ipsam dolorem consequatur doloremque exercitationem, laborum
+      fugiat. Praesentium, ipsa! Illo ullam adipisci praesentium provident aut.
+      Iure, mollitia. Voluptas ipsum beatae reiciendis.
+    </p>
+  );
+
   return (
     <div>
-      <Modal />
+      <Button primary onClick={handleClick}>
+        Open Modal
+      </Button>
+      {showModal && modal}
+
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+
+      <Button primary onClick={handleClick}>
+        Open Modal
+      </Button>
+      {showModal && modal}
     </div>
   );
 };
