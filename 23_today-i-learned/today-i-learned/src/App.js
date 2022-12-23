fb06ba70793cd2adef1643b3fe64d92eaa890729
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-12-23 09:10:38 +0900

    23_today-i-learned

--- 23_today-i-learned/today-i-learned/src/App.js
+++ 23_today-i-learned/today-i-learned/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { supabase } from './api/supabase';
 
 import Header from './components/Header';
 import NewFactForm from './components/NewFactForm';
@@ -16,46 +17,17 @@ const CATEGORIES = [
   { name: 'News', value: 'news', color: '#8b5cf6' },
 ];
 
-const initialFacts = [
-  {
-    id: 1,
-    text: 'React is being developed by Meta (formerly facebook)',
-    source: 'https://opensource.fb.com/',
-    category: 'technology',
-    votes_interesting: 24,
-    votes_mindblowing: 9,
-    votes_false: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: 'Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%',
-    source:
-      'https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids',
-    category: 'society',
-    votes_interesting: 11,
-    votes_mindblowing: 2,
-    votes_false: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: 'Lisbon is the capital of Portugal',
-    source: 'https://en.wikipedia.org/wiki/Lisbon',
-    category: 'society',
-    votes_interesting: 8,
-    votes_mindblowing: 3,
-    votes_false: 1,
-    createdIn: 2015,
-  },
-];
-
 const App = () => {
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
 
   useEffect(() => {
-    setFacts(initialFacts);
+    const getFacts = async () => {
+      const { data } = await supabase.from('facts').select('*');
+      setFacts(data);
+    };
+
+    getFacts();
   }, []);
 
   const handleToggle = () => {
