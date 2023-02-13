8be36cb37301c5b38726c70cad596f6027fe370b
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2023-02-13 14:35:54 +0900

    AuthButtons.test.js

--- 27_rtl_codesplain/src/components/auth/AuthButtons.test.js
+++ 27_rtl_codesplain/src/components/auth/AuthButtons.test.js
@@ -3,12 +3,17 @@ import { MemoryRouter } from 'react-router-dom';
 import { createServer } from '../../test/server';
 import AuthButtons from './AuthButtons';
 
-function renderComponent() {
+async function renderComponent() {
   render(
     <MemoryRouter>
       <AuthButtons />
     </MemoryRouter>
   );
+
+  // Act warning がでるので、linkが表示されるのを、findAllByRole で待つようにする。
+  // Warning: An update to AuthButtons inside a test was not wrapped in act(...).
+  // When testing, code that causes React state updates should be wrapped into act(...):
+  await screen.findAllByRole('link');
 }
 
 describe('when user is not signed in', () => {
@@ -22,17 +27,31 @@ describe('when user is not signed in', () => {
   ]);
 
   test('sigin and signup are visible', async () => {
-    renderComponent();
+    await renderComponent();
 
-    // Act warning がでるので、linkが表示されるのを、findAllByRole で待つようにする。
-    // Warning: An update to AuthButtons inside a test was not wrapped in act(...).
-    // When testing, code that causes React state updates should be wrapped into act(...):
-    await screen.findAllByRole('link');
+    const signIn = screen.getByRole('link', {
+      name: /sign in/i,
+    });
+
+    const signUp = screen.getByRole('link', {
+      name: /sign up/i,
+    });
+
+    expect(signIn).toBeInTheDocument();
+    expect(signIn).toHaveAttribute('href', '/signin');
+
+    expect(signUp).toBeInTheDocument();
+    expect(signUp).toHaveAttribute('href', '/signup');
   });
 
   test('sigout is not visible', async () => {
-    renderComponent();
-    await screen.findAllByRole('link');
+    await renderComponent();
+
+    const signOut = screen.queryByRole('link', {
+      name: /sign out/i,
+    });
+
+    expect(signOut).not.toBeInTheDocument();
   });
 });
 
@@ -47,12 +66,10 @@ describe('when user is signed in', () => {
   ]);
 
   test('sigin and signup are not visible', async () => {
-    renderComponent();
-    await screen.findAllByRole('link');
+    await renderComponent();
   });
 
   test('sigout is visible', async () => {
-    renderComponent();
-    await screen.findAllByRole('link');
+    await renderComponent();
   });
 });
