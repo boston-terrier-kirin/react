cb1565fea368566606c5cbf5646dc96bf9b9f0eb
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2023-02-13 18:42:21 +0900

    テストケース間のキャッシュのリセット

--- 27_rtl_codesplain/src/components/auth/AuthButtons.test.js
+++ 27_rtl_codesplain/src/components/auth/AuthButtons.test.js
@@ -1,13 +1,16 @@
 import { screen, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { SWRConfig } from 'swr';
 import { createServer } from '../../test/server';
 import AuthButtons from './AuthButtons';
 
 async function renderComponent() {
   render(
-    <MemoryRouter>
-      <AuthButtons />
-    </MemoryRouter>
+    <SWRConfig value={{ provider: () => new Map() }}>
+      <MemoryRouter>
+        <AuthButtons />
+      </MemoryRouter>
+    </SWRConfig>
   );
 
   // Act warning がでるので、linkが表示されるのを、findAllByRole で待つようにする。
@@ -20,13 +23,15 @@ describe('when user is not signed in', () => {
   createServer([
     {
       path: '/api/user',
-      res: () => ({
-        user: null,
-      }),
+      res: () => {
+        console.log('NOT loggein request');
+        return { user: null };
+      },
     },
   ]);
 
   test('sigin and signup are visible', async () => {
+    console.log('STRAT TEST 1');
     await renderComponent();
 
     const signIn = screen.getByRole('link', {
@@ -59,17 +64,37 @@ describe('when user is signed in', () => {
   createServer([
     {
       path: '/api/user',
-      res: () => ({
-        user: { id: 3, email: 'test@test.com' },
-      }),
+      res: () => {
+        console.log('★loggein request');
+        return { user: { id: 3, email: 'test@test.com' } };
+      },
     },
   ]);
 
   test('sigin and signup are not visible', async () => {
+    console.log('STRAT TEST 2');
     await renderComponent();
+
+    const signIn = screen.queryByRole('link', {
+      name: /sign in/i,
+    });
+
+    const signUp = screen.queryByRole('link', {
+      name: /sign up/i,
+    });
+
+    expect(signIn).not.toBeInTheDocument();
+    expect(signUp).not.toBeInTheDocument();
   });
 
   test('sigout is visible', async () => {
     await renderComponent();
+
+    const signOut = screen.getByRole('link', {
+      name: /sign out/i,
+    });
+
+    expect(signOut).toBeInTheDocument();
+    expect(signOut).toHaveAttribute('href', '/signout');
   });
 });
