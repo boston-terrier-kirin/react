6a2f645cd679b3fb895a78d482b1d50c1f8c0493
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2023-02-21 06:15:22 +0900

    33_todo-ts

--- 33_todo-ts/backend/src/tasks/taks.route.ts
+++ 33_todo-ts/backend/src/tasks/taks.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { tasksController } from './tasks.controller';
-import { createValidator } from './tasks.validator';
+import { createValidator, updateValidator } from './tasks.validator';
 
 export const tasksRouter: Router = Router();
 
 tasksRouter.get('/', tasksController.getAll);
 
 tasksRouter.post('/', createValidator, tasksController.create);
+
+tasksRouter.put('/', updateValidator, tasksController.update);
--- 33_todo-ts/backend/src/tasks/tasks.controller.ts
+++ 33_todo-ts/backend/src/tasks/tasks.controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
-import { instanceToPlain } from 'class-transformer';
+import { instanceToPlain, plainToInstance } from 'class-transformer';
 import { AppDataSource } from '../..';
 import { Task } from './tasks.entity';
+import { UpdateResult } from 'typeorm';
 
 class TasksController {
   public async getAll(req: Request, res: Response): Promise<Response> {
@@ -46,6 +47,43 @@ class TasksController {
       const plain = instanceToPlain(newTask) as Task[];
 
       return res.status(200).json(plain);
+    } catch (err) {
+      return res.status(500).json({
+        error: 'Internal Server Error',
+      });
+    }
+  }
+
+  public async update(req: Request, res: Response): Promise<Response> {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+        });
+      }
+
+      const { id, status } = req.body;
+      const taskRepository = AppDataSource.getRepository(Task);
+      const task = await taskRepository.findOne({
+        where: { id },
+      });
+
+      if (!task) {
+        return res.status(404).json({
+          error: 'Task not found',
+        });
+      }
+
+      const updatedTask: UpdateResult = await taskRepository.update(
+        id,
+        plainToInstance(Task, { status })
+      );
+
+      const plain = instanceToPlain(updatedTask) as Task[];
+
+      return res.json(plain);
     } catch (err) {
       return res.status(500).json({
         error: 'Internal Server Error',
